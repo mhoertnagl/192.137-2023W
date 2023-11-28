c9b9eccaa405c2a81e39ba4e43bdf7ba76ae8b67
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2023-11-28 22:03:31 +0100

    random, first, best

--- pa/src/neighborhoods.py
+++ pa/src/neighborhoods.py
@@ -1,151 +1,151 @@
 import random
 from abc import ABC, abstractmethod
+from enum import Enum
+
 import numpy as np
 
 from solution import Solution
 
 
+class Improvement(Enum):
+    RANDOM = 1
+    FIRST = 2
+    BEST = 3
+
+
 class Neighborhood(ABC):
 
-    @abstractmethod
+    def __init__(self, improve: Improvement = Improvement.RANDOM):
+        self.improve = improve
+
     def choose(self, sol: Solution) -> Solution:
+        match self.improve:
+            case Improvement.RANDOM:
+                return self.choose_random(sol)
+            case Improvement.FIRST:
+                return self.choose_first(sol)
+            case Improvement.BEST:
+                return self.choose_best(sol)
+        return sol
+
+    @abstractmethod
+    def choose_random(self, sol: Solution) -> Solution:
+        pass
+
+    @abstractmethod
+    def choose_first(self, sol: Solution) -> Solution:
+        pass
+
+    @abstractmethod
+    def choose_best(self, sol: Solution) -> Solution:
         pass
 
 
-class UnredundantNeighborhood(Neighborhood, ABC):
+class TwoFlipNeighborhood(Neighborhood, ABC):
 
-    def choose(self, sol: Solution) -> Solution:
-        for c in sol.get_components():
-            b = len(c) - sol.prob.s
-            for u in c:
-                for v in c:
-                    if self.redundant(sol, b, u, v):
-                        sol.remove_edge(u, v)
-                    if sol.edge_deleted(u, v):
-                        sol.add_edge(u, v)
+    def __init__(self, improve: Improvement = Improvement.RANDOM):
+        super().__init__(improve)
+        self.improve = improve
+
+    def choose_random(self, sol: Solution) -> Solution:
+        new_sol = sol.copy()
+        edges = sol.prob.all_edges()
+        random.shuffle(edges)
+        (i, j), (k, l) = edges[0], edges[1]
+        new_sol.toggle_edge(i, j)
+        new_sol.toggle_edge(k, l)
+        return new_sol if new_sol.is_feasible() else sol
+
+    def choose_first(self, sol: Solution) -> Solution:
+        edges = sol.prob.all_edges_weighted()
+        for x in range(0, len(edges)):
+            for y in range(x+1, len(edges)):
+                (_, i, j), (_, k, l) = edges[x], edges[y]
+                new_sol = sol.copy()
+                new_sol.toggle_edge(i, j)
+                new_sol.toggle_edge(k, l)
+                if new_sol.is_feasible():
+                    if new_sol.get_value() < sol.get_value():
+                        return new_sol
         return sol
 
-    def redundant(self, sol: Solution, b: int, u: int, v: int):
-        du, dv = sol.degree(u), sol.degree(v)
-        return u != v and du > b and dv > b and sol.edge_added(u, v)
-
-
-class TwoFlipNeighborhood(Neighborhood, ABC):
-    
-     def __init__(self, step_fun: str = "first improvement"):
-        self.step_fun = step_fun
-
-     def choose(self, sol: Solution) -> Solution:
-         new_sol = sol.copy()
-         bi,bj,bk,bl = (0,0,0,0)
-         improvement_found = False
-         if self.step_fun == "random":
-            i,j = sol.random_edge_from_all()
-            k, l = sol.random_edge_from_all()
-            while i == k and j == l:
-                k, l = sol.random_edge_from_all()
-            test_sol = sol.copy()
-            test_sol.toggle_edge(i, j)
-            test_sol.toggle_edge(k, l)
-            if test_sol.is_feasible():
-                return test_sol          
-            else:
-                return new_sol
-         for index,(_, i, j) in enumerate(sol.prob.all_edges_weighted()):
-             # print(i,j)
-             for index2,(_, k, l) in enumerate(sol.prob.all_edges_weighted()):
-                 if index2 > index:
-                 # if i != k or j != l:
-                     test_sol = sol.copy()
-                     test_sol.toggle_edge(i, j)
-                     test_sol.toggle_edge(k, l)
-                     if test_sol.is_feasible():
-                         if test_sol.get_value() < new_sol.get_value():
-                             if self.step_fun == "best improvement":
-                                 new_sol = test_sol.copy()
-                                 bi,bj,bk,bl = i,j,k,l
-                                 improvement_found = True
-                             if self.step_fun == "first improvement":
-                                 print("first improvement found for:",i,j,k,l)
-                                 new_sol = test_sol.copy()
-                                 improvement_found = True
-                                 return new_sol
-         if improvement_found:
-            print("best improvement found for:",bi,bj,bk,bl) 
-         else:
-             print("no improvement found")
-         return new_sol
+    def choose_best(self, sol: Solution) -> Solution:
+        edges = sol.prob.all_edges_weighted()
+        for x in range(0, len(edges)):
+            for y in range(x+1, len(edges)):
+                (_, i, j), (_, k, l) = edges[x], edges[y]
+                new_sol = sol.copy()
+                new_sol.toggle_edge(i, j)
+                new_sol.toggle_edge(k, l)
+                if new_sol.is_feasible():
+                    if new_sol.get_value() < sol.get_value():
+                        sol = new_sol
+        return sol
 
 
 class TwoExchangeNeighborhood(Neighborhood, ABC):
 
-    def choose(self, sol: Solution) -> Solution:
-        cs = sol.get_components()
-        # Consider all components of length at
-        # least 4. Then the components have at
-        # least 3 edges.
-        cs = list(filter(lambda x: len(x) >= 4, cs))
-        # Pick one of the components randomly.
-        # TODO: We could do this multiple times
-        #       in a single choose operation.
+    def __init__(self, improve: Improvement = Improvement.RANDOM):
+        super().__init__(improve)
+        self.improve = improve
+
+    def choose_random(self, sol: Solution) -> Solution:
+        cs = [c for c in sol.get_components() if len(c) >= 4]
         c = cs[np.random.randint(0, len(cs))]
-        # Get all edges for this component.
         es = sol.get_edges(c)
-        # Shuffle the edges, then take the first
-        # two edges.
         random.shuffle(es)
-        (x1, y1) = es[0]
-        (x2, y2) = es[1]
+        (x1, y1), (x2, y2) = es[0], es[1]
         new_sol = sol.copy()
-        # Make sure that the cross-edges do not exist.
         if not sol.has_edge(x1, y2) and not sol.has_edge(x2, y1):
-            # Remove the chosen edges, ...
             new_sol.remove_edge(x1, y1)
             new_sol.remove_edge(x2, y2)
-            # ... then add the cross-edges.
             new_sol.add_edge(x1, y2)
             new_sol.add_edge(x2, y1)
             return new_sol
         return sol
 
+    def choose_first(self, sol: Solution) -> Solution:
+        cs = [c for c in sol.get_components() if len(c) >= 4]
+        for c in cs:
+            es = sol.get_edges(c)
+            for x in range(0, len(es)):
+                for y in range(x+1, len(es)):
+                    (x1, y1), (x2, y2) = es[x], es[y]
+                    new_sol = sol.copy()
+                    if not sol.has_edge(x1, y2) and not sol.has_edge(x2, y1):
+                        new_sol.remove_edge(x1, y1)
+                        new_sol.remove_edge(x2, y2)
+                        new_sol.add_edge(x1, y2)
+                        new_sol.add_edge(x2, y1)
+                        if new_sol.get_value() < sol.get_value():
+                            return new_sol
+        return sol
 
-class SingleComponentMultiExchangeNeighborhood(Neighborhood, ABC):
-
-    def choose(self, sol: Solution) -> Solution:
-        cs = sol.get_components()
-        # Consider all components of length at
-        # least 4. Then the components have at
-        # least 3 edges.
-        cs = list(filter(lambda x: len(x) >= 4, cs))
-        # Pick one of the components randomly.
-        # TODO: We could do this multiple times
-        #       in a single choose operation.
-        c = cs[np.random.randint(0, len(cs))]
-        # Get all edges for this component.
-        es = sol.get_edges(c)
-        # Shuffle the edges, then take the first
-        # two edges.
-        random.shuffle(es)
-        new_sol = sol.copy()
-        # Iterate over all edges.
-        for i in range(0, len(es)-1):
-            (x1, y1) = es[i]
-            (x2, y2) = es[i+1]
-            # Make sure that the cross-edges do not exist.
-            if not sol.has_edge(x1, y2) and not sol.has_edge(x2, y1):
-                # Remove the chosen edges, ...
-                new_sol.remove_edge(x1, y1)
-                new_sol.remove_edge(x2, y2)
-                # ... then add the cross edges.
-                new_sol.add_edge(x1, y2)
-                new_sol.add_edge(x2, y1)
-                # Stop with the first successful exchange.
-        return new_sol
+    def choose_best(self, sol: Solution) -> Solution:
+        cs = [c for c in sol.get_components() if len(c) >= 4]
+        for c in cs:
+            es = sol.get_edges(c)
+            for x in range(0, len(es)):
+                for y in range(x+1, len(es)):
+                    (x1, y1), (x2, y2) = es[x], es[y]
+                    new_sol = sol.copy()
+                    if not sol.has_edge(x1, y2) and not sol.has_edge(x2, y1):
+                        new_sol.remove_edge(x1, y1)
+                        new_sol.remove_edge(x2, y2)
+                        new_sol.add_edge(x1, y2)
+                        new_sol.add_edge(x2, y1)
+                        if new_sol.get_value() < sol.get_value():
+                            sol = new_sol
+        return sol
 
 
 class VertexMoveNeighborhood(Neighborhood, ABC):
 
-    def choose(self, sol: Solution) -> Solution:
+    def __init__(self, improve: Improvement = Improvement.RANDOM):
+        super().__init__(improve)
+        self.improve = improve
+
+    def choose_random(self, sol: Solution) -> Solution:
         c1 = list(sol.get_random_component())
         c2 = list(sol.get_random_component())
         v = c1[np.random.randint(0, len(c1))]
@@ -159,10 +159,46 @@ class VertexMoveNeighborhood(Neighborhood, ABC):
                 new_sol.add_edge(u, v)
         return new_sol
 
+    def choose_first(self, sol: Solution) -> Solution:
+        cs = [list(c) for c in sol.get_components()]
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                for v in c1:
+                    new_sol = sol.copy()
+                    for u in sol.get_neighbors(v):
+                        new_sol.remove_edge(u, v)
+                    for u in c2:
+                        if u != v:
+                            new_sol.add_edge(u, v)
+                    if new_sol.get_value() < sol.get_value():
+                        return new_sol
+        return sol
+
+    def choose_best(self, sol: Solution) -> Solution:
+        cs = [list(c) for c in sol.get_components()]
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                for v in c1:
+                    new_sol = sol.copy()
+                    for u in sol.get_neighbors(v):
+                        new_sol.remove_edge(u, v)
+                    for u in c2:
+                        if u != v:
+                            new_sol.add_edge(u, v)
+                    if new_sol.get_value() < sol.get_value():
+                        sol = new_sol
+        return sol
+
 
 class VertexSwapNeighborhood(Neighborhood, ABC):
 
-    def choose(self, sol: Solution) -> Solution:
+    def __init__(self, improve: Improvement = Improvement.RANDOM):
+        super().__init__(improve)
+        self.improve = improve
+
+    def choose_random(self, sol: Solution) -> Solution:
         c1 = list(sol.get_random_component())
         c2 = list(sol.get_random_component())
         v1 = c1[np.random.randint(0, len(c1))]
@@ -170,15 +206,11 @@ class VertexSwapNeighborhood(Neighborhood, ABC):
         new_sol = sol.copy()
         ns1 = list(sol.get_neighbors(v1))
         ns2 = list(sol.get_neighbors(v2))
-        # Remove edges of v1 to old
-        # component and add edges to v2.
         for u in ns1:
             if new_sol.has_edge(u, v1):
                 new_sol.remove_edge(u, v1)
             if u != v2:
                 new_sol.add_edge(u, v2)
-        # Remove edges of v2 to old
-        # component and add edges to v1.
         for u in ns2:
             if new_sol.has_edge(u, v2):
                 new_sol.remove_edge(u, v2)
@@ -186,21 +218,100 @@ class VertexSwapNeighborhood(Neighborhood, ABC):
                 new_sol.add_edge(u, v1)
         return new_sol
 
+    def choose_first(self, sol: Solution) -> Solution:
+        cs = [list(c) for c in sol.get_components()]
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                for v1 in c1:
+                    for v2 in c2:
+                        new_sol = sol.copy()
+                        ns1 = list(sol.get_neighbors(v1))
+                        ns2 = list(sol.get_neighbors(v2))
+                        for u in ns1:
+                            if new_sol.has_edge(u, v1):
+                                new_sol.remove_edge(u, v1)
+                            if u != v2:
+                                new_sol.add_edge(u, v2)
+                        for u in ns2:
+                            if new_sol.has_edge(u, v2):
+                                new_sol.remove_edge(u, v2)
+                            if u != v1:
+                                new_sol.add_edge(u, v1)
+                        if new_sol.get_value() < sol.get_value():
+                            return new_sol
+        return sol
+
+    def choose_best(self, sol: Solution) -> Solution:
+        cs = [list(c) for c in sol.get_components()]
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                for v1 in c1:
+                    for v2 in c2:
+                        new_sol = sol.copy()
+                        ns1 = list(sol.get_neighbors(v1))
+                        ns2 = list(sol.get_neighbors(v2))
+                        for u in ns1:
+                            if new_sol.has_edge(u, v1):
+                                new_sol.remove_edge(u, v1)
+                            if u != v2:
+                                new_sol.add_edge(u, v2)
+                        for u in ns2:
+                            if new_sol.has_edge(u, v2):
+                                new_sol.remove_edge(u, v2)
+                            if u != v1:
+                                new_sol.add_edge(u, v1)
+                        if new_sol.get_value() < sol.get_value():
+                            sol = new_sol
+        return sol
+
 
 class ComponentMergeNeighborhood(Neighborhood, ABC):
 
-    def __init__(self, k_max: int = 10):
+    def __init__(self, improve: Improvement = Improvement.RANDOM, k_max: int = 10):
+        super().__init__(improve)
+        self.improve = improve
         self.k_max = k_max
 
-    def choose(self, sol: Solution) -> Solution:
+    def choose_random(self, sol: Solution) -> Solution:
         cs = [c for c in sol.get_components() if len(c) <= self.k_max]
         if len(cs) < 2:
             return sol
         random.shuffle(cs)
-        c1 = cs[0]
-        c2 = cs[1]
+        c1, c2 = cs[0], cs[1]
         new_sol = sol.copy()
         for v1 in c1:
             for v2 in c2:
                 new_sol.add_edge(v1, v2)
         return new_sol
+
+    def choose_first(self, sol: Solution) -> Solution:
+        cs = [c for c in sol.get_components() if len(c) <= self.k_max]
+        if len(cs) < 2:
+            return sol
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                new_sol = sol.copy()
+                for v1 in c1:
+                    for v2 in c2:
+                        new_sol.add_edge(v1, v2)
+                if new_sol.get_value() < sol.get_value():
+                    return new_sol
+        return sol
+
+    def choose_best(self, sol: Solution) -> Solution:
+        cs = [c for c in sol.get_components() if len(c) <= self.k_max]
+        if len(cs) < 2:
+            return sol
+        for x in range(0, len(cs)):
+            for y in range(x+1, len(cs)):
+                c1, c2 = cs[x], cs[y]
+                new_sol = sol.copy()
+                for v1 in c1:
+                    for v2 in c2:
+                        new_sol.add_edge(v1, v2)
+                if new_sol.get_value() < sol.get_value():
+                    sol = new_sol
+        return sol
