a0820d15c074224262efaeb52123e8c0ce7f3b5f
Martin Fischer <martin.fischer@tuwien.ac.at>
2023-12-01 13:09:06 +0100

    localsearchtuning

--- pa/src/localsearch.py
+++ pa/src/localsearch.py
@@ -21,3 +21,29 @@ class LocalSearch:
             if new_sol.get_value() <= sol.get_value():
                 sol = new_sol.copy()
         return sol
+
+
+class LocalSearchTuning:
+
+    def __init__(self, nbh: Neighborhood, ter: Termination):
+        self.nbh = nbh
+        self.ter = ter
+
+    def run(self, sol: Solution) -> Solution:
+        self.ter.init()
+        c = 0
+        f = []
+        lo = 0
+        while True:
+            c += 1
+            new_sol = self.nbh.choose(sol)
+            if self.ter.done(sol, new_sol):
+                break
+            if new_sol.get_value() <= sol.get_value():
+                sol = new_sol.copy()
+                f.append(sol.get_value())
+                lo = c
+        return sol,c,f,lo
+    
+    
+    
